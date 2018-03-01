176ea94c5b3a1ccce788cf12985f446e62c1ec07
RyanCastelline <35778562+RyanCastelline@users.noreply.github.com>
2018-02-28 19:46:50 -0600

    Altered SampleDao class to display more complicated SQL statement.

--- Billing/src/main/java/billingDaos/SampleDao.java
+++ Billing/src/main/java/billingDaos/SampleDao.java
@@ -13,17 +13,26 @@ public class SampleDao {
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/kinetic_fitness?autoReconnect=true&useSSL=false",
 				"root",	"Caste-dude1");
 		//jdbc:mysql://:localhost:3306/kinetic_fitness is the url
+		//?autoReconnect=true&useSSL=false is used to bypass a warning issued while accessing the DB.
 		//"root" is the user and password for the DB.
+		//Enter your profile password for the last argument within the double quotes.
 		
-		//Preparing sql statement to access database.
-		PreparedStatement statement = con.prepareStatement("select * from client");
+		//Preparing sql statement to access database, determining each student and total drop-in classes.
+		PreparedStatement statement = con.prepareStatement(
+				"select client.FIRST_NAME, client.LAST_NAME, (client.CLASSES_TAKEN - membership.classes_per_month) as 'DROPIN_CLASSES'\r\n" + 
+				"from client\r\n" + 
+				"left join membership on client.MEMBERSHIP_TIER = membership.membership_tier\r\n" + 
+				"order by client.LAST_NAME");
 		
 		//Creates variable to execute query.
 		ResultSet result = statement.executeQuery();
 		
+		System.out.println("The following students have drop-in classes:");
 		while(result.next()) {
-			System.out.println("Client #: " + result.getString(1) + " Last name: " + result.getString(2) + 
-					" First name: " + result.getString(3));
+			if(result.getInt(3) > 0) { //If statement only displays students with positive drop-ins.
+				System.out.println("Name: " + result.getString(1) + " " + result.getString(2) + 
+						"; Total drop-in classes: " + result.getInt(3));
+			}
 		}
 	}
 }
